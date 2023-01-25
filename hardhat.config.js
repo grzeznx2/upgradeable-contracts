161f2653e3b125a61b78c664fbe7cfd23a966bf0
grzeznx2 <grzeznx2@gmail.com>
2023-01-25 18:33:23 +0100

    Prepare for Box deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
+require('dotenv').config()
+
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
